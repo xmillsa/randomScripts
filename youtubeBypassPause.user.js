b6b283a45fd5e3767c2f5a035ca2e87166577255
Andy Mills <millsa123@hotmail.co.uk>
2021-01-31 11:19:52 +0000

    Create youtubeBypassPause.user.js

---
+++ youtubeBypassPause.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        Youtube Bypass Pause
+// @version     0.1.0
+// @namespace   https://andys-net.co.uk/
+// @homepageURL https://andys-net.co.uk/
+// @match       https://www.youtube.com/*
+// @license     GPL-3.0-or-later; https://spdx.org/licenses/GPL-3.0-or-later.html
+// @author      Xmillsa
+// @description Auto click "Are you still watching?" popup.
+// @grant       none
+// ==/UserScript==
+
+(function(){
+    'use strict';
+    const loop = () => {
+        let els = document.querySelectorAll( '.line-text.style-scope.yt-confirm-dialog-renderer' );
+        if ( els.length > 0 ){
+            els.forEach( el => {
+                if ( el.innerText == 'Video paused. Continue watching?' ){
+                    el.parentNode.parentNode.querySelector( '#confirm-button' ).click();
+                }
+            });
+        }
+    };
+    setInterval( loop, 75 );
+})();
